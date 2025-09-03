9b41f8862afdeba09fd49aaba9d2e0cb43014994
Moso2006-gh <mateo.martin.nieto@gmail.com>
2025-09-03 18:45:09 +0200

    21 part1 done

--- 2024/21.py
+++ 2024/21.py
@@ -1,71 +1,109 @@
-raw_data = """341A
-803A
-149A
-683A
-208A"""
+import itertools
+import numpy as np
+from pathlib import Path
 
-robot_1_coords = {
-    "9": [0, 2],
-    "8": [0, 1],
-    "7": [0, 0],
-    "6": [1, 2],
-    "5": [1, 1],
-    "4": [1, 0],
-    "3": [2, 2],
-    "2": [2, 1],
-    "1": [2, 0],
-    "0": [3, 1],
-    "A": [3, 2]
+input = open(Path(__file__).parent / "21.txt").read().split("\n")
+
+kepad_robot_coords = {
+    "9": np.array([0, 2]),
+    "8": np.array([0, 1]),
+    "7": np.array([0, 0]),
+    "6": np.array([1, 2]),
+    "5": np.array([1, 1]),
+    "4": np.array([1, 0]),
+    "3": np.array([2, 2]),
+    "2": np.array([2, 1]),
+    "1": np.array([2, 0]),
+    "0": np.array([3, 1]),
+    "A": np.array([3, 2])
 }
-robot_2_3_coords = {
-    "^": [0, 1],
-    "<": [1, 0],
-    "v": [1, 1],
-    ">": [1, 2],
-    "A": [0, 2]
+robot_coords = {
+    "^": np.array([0, 1]),
+    "<": np.array([1, 0]),
+    "v": np.array([1, 1]),
+    ">": np.array([1, 2]),
+    "A": np.array([0, 2])
 }
 
-first_level_incoding = []
+def get_move(diff):
+    move_vertical = ""
+    if diff[0] < 0:
+        move_vertical = "^"
+    else:
+        move_vertical = "v"
+    move_vertical *= abs(diff[0])
 
-def encode(code, robot_initial_pos, coords, hole_id):
-    possible_sequences = [""]
-    robot = robot_initial_pos
-    for i, char in enumerate(list(code)):
-        new_possible_sequences = []
+    move_horizontal = ""
+    if diff[1] < 0:
+        move_horizontal = "<"
+    else:
+        move_horizontal = ">"
+    move_horizontal *= abs(diff[1])
 
-        char_coord = coords[char]
-        move = [char_coord[0] - robot[0], char_coord[1] - robot[1]]
-        vertical_seq = ("^" if move[0] < 0 else "v") * abs(move[0])
-        horizontal_seq = (">" if move[1] > 0 else "<") * abs(move[1])
+    return move_vertical, move_horizontal
 
-        if (hole_id == 0 and move[0] > 0 and robot[0] == 0) or (hole_id == 1 and move[0] < 0 and char_coord[1] == 0 and robot[0] == 3):
-            for sequence in possible_sequences:
-                if (sequence + vertical_seq + horizontal_seq + "A") not in new_possible_sequences:
-                    new_possible_sequences.append(sequence + vertical_seq + horizontal_seq + "A")
-        else:
-            for sequence in possible_sequences:
-                if (sequence + vertical_seq + horizontal_seq + "A") not in new_possible_sequences:
-                new_possible_sequences.append(sequence + vertical_seq + horizontal_seq + "A")
-                new_possible_sequences.append(sequence + horizontal_seq + vertical_seq + "A")
-            
-        possible_sequences = new_possible_sequences
-        robot = char_coord
-    return possible_sequences
+def get_robot_paths(path):    
+    robot = "A"
+    posible_paths = []
+    for char in path:
+        diff = robot_coords[char] - robot_coords[robot]
+        move_vertical, move_horizontal = get_move(diff)
+        char_paths = set()
+        if not ((robot == "^" or robot == "A") and char == "<"):
+            char_paths.add(move_horizontal + move_vertical + "A")
+        if not ((char == "^" or char == "A") and robot == "<"):
+            char_paths.add(move_vertical + move_horizontal + "A")
+        
+        posible_paths.append(list(char_paths))        
+        robot = char
+    return posible_paths
+
+def get_all_paths(posible_paths):
+    return [''.join(p) for p in itertools.product(*posible_paths)]
+
+def apply_robot(paths):
+    all_robot_paths = []
+    shortest = np.inf
+    for path in paths:
+        robot_paths = get_all_paths(get_robot_paths(path))
+        for robot_path in robot_paths:
+            if len(robot_path) < shortest:
+                all_robot_paths = [robot_path]
+                shortest = len(robot_path)
+            elif len(robot_path) == shortest:
+                all_robot_paths.append(robot_path)
+    return all_robot_paths
 
 sol = 0
-for code in raw_data.split("\n"):
+for code in input:
     print(code)
-    smallest_length = float("inf")
-    first_encoding_sequences = encode(code, [3, 2], robot_1_coords, 1)
-    for sequence in first_encoding_sequences:
-        second_encoding_sequences = encode(sequence, [0, 2], robot_2_3_coords, 0)
+    kepad_robot = "A"
+    length_of_seq = 0
+    for char in code:
+        diff = kepad_robot_coords[char] - kepad_robot_coords[kepad_robot]
+        
+        move_vertical, move_horizontal = get_move(diff)
 
-        for sequence_2 in second_encoding_sequences:
-            third_encoding_sequences = encode(sequence_2, [0, 2], robot_2_3_coords, 0)
-            for sequence_3 in third_encoding_sequences:
-                if smallest_length > len(sequence_3):
-                    smallest_length = len(sequence_3)
+        posible = set()
+        if not (kepad_robot in ["7", "4", "1"] and char in ["0", "A"]):
+            posible.add(move_vertical + move_horizontal + "A")
+        if not (char in ["7", "4", "1"] and kepad_robot in ["0", "A"]):
+            posible.add(move_horizontal + move_vertical + "A")
+        kepad_robot = char
 
-    sol += int(code.split("A")[0]) * smallest_length
-print(sol)       
+        robot_paths = posible
+        for i in range(2):
+            robot_paths = apply_robot(robot_paths)
+            print(robot_paths[0])
 
+        shortest = np.inf
+        for path in robot_paths:
+            if len(path) < shortest:
+                shortest = len(path)
+
+        length_of_seq += shortest
+    sol += length_of_seq * int(code[:-1])
+
+print(sol)
+
+# <vA <A A >>^A vA A <^A >A   v<<A >>^A vA ^A     v<<A >>^A A <vA >A ^A ((vA<A^>A))<A>A  v<<A >A ^>A (<vA<A>>^A)AA<Av>A^AvA<^A>A
